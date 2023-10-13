257200a86a15172bcb489255a65303a4774e5c70
Benjisparks <146876239+Benjisparks@users.noreply.github.com>
2023-10-13 12:17:20 -0600

    started basic code for ball objects

--- 04-Array-Assignment/sketch.js
+++ 04-Array-Assignment/sketch.js
@@ -5,11 +5,48 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let balls = [];
+
 function setup() {
-  createCanvas(windowWidth/2, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+  displayBall();
 }
 
+function createBall(){
+  let theBall = {
+    x: mouseX,
+    y: mouseY,
+    radius: 30,
+  };
+  balls.push(theBall);
+}
+
+// function usercreateBall(){
+//   let theBall = {
+//     x: mouseX,
+//     y: mouseY,
+//     radius: 30,
+//     color: "orange",
+//   };
+//   return theBall;
+// }
+
+function mouseClicked(){
+  let userBall = createBall();
+  displayBall(userBall);
+}
+
+// function mouseClicked(){
+//   someBall = usercreateBall();
+//   displayBall();
+// }
+
+function displayBall(){
+  for(let someBall of balls){
+    circle(someBall.x,someBall.y,someBall.radius*2);
+  }
+}
