9649f4aa10f81c0c95cff08fec1ddd366c0a0473
Benjisparks <146876239+Benjisparks@users.noreply.github.com>
2023-11-02 11:15:01 -0600

    added new function

--- 14-grid-game/sketch.js
+++ 14-grid-game/sketch.js
@@ -5,8 +5,10 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let GRID_SIZE = 16;
 let titleFont;
 let theBackground;
+let gmScreen = "start";
 
 function preload(){
   titleFont = loadFont("1989.ttf");
@@ -19,4 +21,26 @@ function setup() {
 
 function draw() {
   background(220);
+}
+
+function generateGrid(cols,rows){
+  let theGrid = [];
+  for(let y = 0; y < cols; y++){
+    theGrid.push([]);
+    for(let x = 0; x < rows; x++){
+      
+    }
+  }
+}
+
+function startText() {
+
+}
+
+function keyTyped(){
+  if( key === ENTER){
+    if(gmScreen === "start"){
+      gmScreen = "game";
+    }
+  }
 }
