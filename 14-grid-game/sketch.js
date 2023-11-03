eb928bb4a8da4dae0418fbf91604fcc2c16b5193
Benjisparks <146876239+Benjisparks@users.noreply.github.com>
2023-11-03 12:17:16 -0600

    Added function

--- 14-grid-game/sketch.js
+++ 14-grid-game/sketch.js
@@ -7,8 +7,11 @@
 
 let GRID_SIZE = 16;
 let titleFont;
+let titleSize;
 let theBackground;
 let gmScreen = "start";
+let grid;
+let cellSize;
 
 function preload(){
   titleFont = loadFont("1989.ttf");
@@ -17,24 +20,58 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = generateGrid(GRID_SIZE,GRID_SIZE);
+  titleSize = width/4;
+  if (height > width) {
+    cellSize = width/GRID_SIZE;
+  }
+  else {
+    cellSize = height/GRID_SIZE;
+  }
 }
 
 function draw() {
   background(220);
+  //displayGrid();
+  startText();
 }
+
+function runGame(){
+  if( gmScreen === "start"){
+    //displayBg();
+    startText();
+  }
+  // else if(gmScreen === "game"){
+  //   playGame();
+  // }
+}
+
+// function playGame(){
+
+// }
 
 function generateGrid(cols,rows){
   let theGrid = [];
   for(let y = 0; y < cols; y++){
     theGrid.push([]);
     for(let x = 0; x < rows; x++){
-      
+      theGrid[y].push(0);
     }
   }
+  return theGrid;
 }
 
-function startText() {
+// function placeMines(){
 
+// }
+
+function displayGrid(){
+  for(let y = 0; y < GRID_SIZE; y++){
+    for(let x = 0; x < GRID_SIZE; x++){
+      fill("white");
+      rect(x*cellSize,y*cellSize,cellSize,cellSize);
+    }
+  }
 }
 
 function keyTyped(){
@@ -43,4 +80,11 @@ function keyTyped(){
       gmScreen = "game";
     }
   }
+}
+
+function startText() {
+  textFont(titleFont);
+  textSize(titleSize);
+  text("MINESWEEPER",width/2,height/4);
+  textAlign(CENTER,CENTER);
 }
